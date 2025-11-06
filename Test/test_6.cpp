e7bc6672add344980e0122d3238eb653947088cf
Sephoration <Epliskey@foxmail.com>
2025-11-06 21:07:22 +0800

    2

--- Test/test_6.cpp
+++ Test/test_6.cpp
@@ -5,34 +5,37 @@
 
 #include <iostream>
 
-int main() {
+int main()
+{
 
     int arr[1000] = {0};
 
     std::cout << "请输入一串整数，以空格分隔，输入非数字字符结束：" << std::endl;
-    
+
     int index = 0;
-    while (std::cin >> arr[index]) { 
+    while (std::cin >> arr[index])
+    {
         index++;
     }
-   
-    // 冒泡排序
-    for (int i = 0; i < index -1 ; i++){   //排除最后一个数
 
-        for (int j = 0; j < (index - i - 1 ) ; j++) 
+    // 冒泡排序
+    for (int i = 0; i < index - 1; i++)
+    { // 排除最后一个数
+
+        for (int j = 0; j < (index - i - 1); j++)
         {
-            if(arr[j] > arr[j+1] )
+            if (arr[j] > arr[j + 1])
             {
                 int temp = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = temp;
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
             }
         }
-        
     }
 
     std::cout << "排序后的结果是：" << std::endl;
-    for (int i = 0; i < index; i++) {
+    for (int i = 0; i < index; i++)
+    {
         std::cout << arr[i] << " ";
     }
 }
