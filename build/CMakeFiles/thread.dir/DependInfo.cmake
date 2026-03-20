b40928b730278a7f6d385702ffcb13e2056cf720
Eeeeeee <Epliskey@foxmail.com>
2026-03-20 15:43:55 +0800

    STL，array、function、uniqueptr的完成

---
+++ build/CMakeFiles/thread.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Git_Code/CPP/Thread/main.c" "CMakeFiles/thread.dir/main.c.obj" "gcc" "CMakeFiles/thread.dir/main.c.obj.d"
+  "C:/Git_Code/CPP/Thread/thrd_pool.c" "CMakeFiles/thread.dir/thrd_pool.c.obj" "gcc" "CMakeFiles/thread.dir/thrd_pool.c.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/thread.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/thread.dir/main.c.obj"
+  "CMakeFiles/thread.dir/main.c.obj.d"
+  "CMakeFiles/thread.dir/thrd_pool.c.obj"
+  "CMakeFiles/thread.dir/thrd_pool.c.obj.d"
+  "libthread.dll.a"
+  "thread.exe"
+  "thread.exe.manifest"
+  "thread.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/thread.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
